ae181b7e49283af07b9e2d28fb7b3424048c4374
espadara <espadara@pirate.capn.gg>
2025-11-02 15:27:02 +0100

    [ADD] printf handlers

---
+++ srcs/sea_printf_handlers.c
@@ -0,0 +1,203 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                        ______                                              */
+/*                     .-"      "-.                                           */
+/*                    /            \                                          */
+/*        _          |              |          _                              */
+/*       ( \         |,  .-.  .-.  ,|         / )                             */
+/*        > "=._     | )(__/  \__)( |     _.=" <                              */
+/*       (_/"=._"=._ |/     /\     \| _.="_.="\_)                             */
+/*              "=._ (_     ^^     _)"_.="                                    */
+/*                  "=\__|IIIIII|__/="                                        */
+/*                 _.="| \IIIIII/ |"=._                                       */
+/*       _     _.="_.="\          /"=._"=._     _                             */
+/*      ( \_.="_.="     `--------`     "=._"=._/ )                            */
+/*       > _.="                            "=._ <                             */
+/*      (_/                                    \_)                            */
+/*                                                                            */
+/*      Filename: sea_printf_handlers.c                                       */
+/*      By: espadara <espadara@pirate.capn.gg>                                */
+/*      Created: 2025/11/02 15:13:25 by espadara                              */
+/*      Updated: 2025/11/02 15:26:32 by espadara                              */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sea_printf.h"
+
+void	sea_handle_char(t_sea_state *state)
+{
+  char	c;
+
+  c = va_arg(state->args, int);
+  if (!(state->flags.bits & FLAG_MINUS))
+    sea_handle_width(state, 1, 0);
+  sea_putchar_fd(c, 1);
+  state->total_len++;
+  if (state->flags.bits & FLAG_MINUS)
+    sea_handle_width(state, 1, 0);
+}
+
+void	sea_handle_string(t_sea_state *state)
+{
+  char	*s;
+  int		len;
+
+    s = va_arg(state->args, char *);
+    if (s == NULL)
+      s = "(null)";
+    len = sea_strlen(s);
+    if ((state->flags.bits & FLAG_HAS_PRECISION)
+        && state->flags.precision < len)
+      len = state->flags.precision;
+    if (!(state->flags.bits & FLAG_MINUS))
+      sea_handle_width(state, len, 0);
+    write(1, s, len);
+    state->total_len += len;
+    if (state->flags.bits & FLAG_MINUS)
+      sea_handle_width(state, len, 0);
+}
+
+void	sea_handle_pointer(t_sea_state *state)
+{
+  unsigned long long	p;
+  char				*s;
+  int					len;
+
+    p = (unsigned long long)va_arg(state->args, void *);
+    s = sea_arena_utoa_base(state, p, "0123456789abcdef");
+    len = sea_strlen(s) + 2;
+    if (!(state->flags.bits & FLAG_MINUS))
+      sea_handle_width(state, len, 0);
+    sea_putstr_fd("0x", 1);
+    sea_putstr_fd(s, 1);
+    state->total_len += len;
+    if (state->flags.bits & FLAG_MINUS)
+      sea_handle_width(state, len, 0);
+}
+
+void	sea_handle_int(t_sea_state *state)
+{
+  long long	n;
+  char		*s;
+  int			len;
+  int			is_neg;
+  int			prefix_len;
+  char		prefix[2];
+
+  n = va_arg(state->args, int);
+  is_neg = (n < 0);
+  s = sea_arena_itoa(state, n);
+  len = sea_strlen(s);
+  if (n == 0 && (state->flags.bits & FLAG_HAS_PRECISION)
+      && state->flags.precision == 0)
+    len = 0;
+  sea_handle_precision(state, &s, &len, is_neg);
+  is_neg = (s[0] == '-');
+  prefix_len = 0;
+  if (is_neg)
+    {
+      prefix[0] = '-';
+      prefix_len = 1;
+      s++;
+      len--;
+    }
+  else if (state->flags.bits & FLAG_PLUS)
+    prefix[prefix_len++] = '+';
+  else if (state->flags.bits & FLAG_SPACE)
+    prefix[prefix_len++] = ' ';
+  if (!(state->flags.bits & FLAG_MINUS))
+    {
+      if (state->flags.bits & FLAG_ZERO)
+        {
+          write(1, prefix, prefix_len);
+          sea_handle_width(state, len + prefix_len, 1);
+        }
+      else
+        {
+          sea_handle_width(state, len + prefix_len, 0);
+          write(1, prefix, prefix_len);
+        }
+    }
+    else
+      write(1, prefix, prefix_len);
+    write(1, s, len);
+    state->total_len += (len + prefix_len);
+    if (state->flags.bits & FLAG_MINUS)
+      sea_handle_width(state, len + prefix_len, 0);
+}
+
+void	sea_handle_unsigned(t_sea_state *state)
+{
+  unsigned int	n;
+  char			*s;
+  int				len;
+
+  n = va_arg(state->args, unsigned int);
+  s = sea_arena_utoa_base(state, n, "0123456789");
+  len = sea_strlen(s);
+  if (n == 0 && (state->flags.bits & FLAG_HAS_PRECISION)
+        && state->flags.precision == 0)
+      len = 0;
+    sea_handle_precision(state, &s, &len, 0);
+    if (!(state->flags.bits & FLAG_MINUS))
+      sea_handle_width(state, len, 1);
+    write(1, s, len);
+    state->total_len += len;
+    if (state->flags.bits & FLAG_MINUS)
+      sea_handle_width(state, len, 0);
+}
+
+void	sea_handle_hex(t_sea_state *state, int is_upper)
+{
+  unsigned int	n;
+  char			*s;
+  char			*base;
+  int				len;
+  int				prefix_len;
+
+  n = va_arg(state->args, unsigned int);
+  if (is_upper)
+    base = "0123456789ABCDEF";
+  else
+    base = "0123456789abcdef";
+  s = sea_arena_utoa_base(state, n, base);
+  len = sea_strlen(s);
+  if (n == 0 && (state->flags.bits & FLAG_HAS_PRECISION)
+      && state->flags.precision == 0)
+    len = 0;
+  if (n == 0)
+    state->flags.bits &= ~FLAG_HASH;
+  sea_handle_precision(state, &s, &len, 0);
+  prefix_len = (state->flags.bits & FLAG_HASH) ? 2 : 0;
+  if (!(state->flags.bits & FLAG_MINUS))
+    {
+      if (state->flags.bits & FLAG_ZERO)
+        {
+          if (prefix_len)
+            (is_upper) ? sea_putstr_fd("0X", 1) : sea_putstr_fd("0x", 1);
+          sea_handle_width(state, len + prefix_len, 1);
+        }
+      else
+        {
+          sea_handle_width(state, len + prefix_len, 0);
+          if (prefix_len)
+            (is_upper) ? sea_putstr_fd("0X", 1) : sea_putstr_fd("0x", 1);
+        }
+    }
+  else if (prefix_len)
+    (is_upper) ? sea_putstr_fd("0X", 1) : sea_putstr_fd("0x", 1);
+  write(1, s, len);
+  state->total_len += (len + prefix_len);
+  if (state->flags.bits & FLAG_MINUS)
+    sea_handle_width(state, len + prefix_len, 0);
+}
+
+void	sea_handle_percent(t_sea_state *state)
+{
+  if (!(state->flags.bits & FLAG_MINUS))
+    sea_handle_width(state, 1, 0);
+  sea_putchar_fd('%', 1);
+  state->total_len++;
+  if (state->flags.bits & FLAG_MINUS)
+      sea_handle_width(state, 1, 0);
+}
